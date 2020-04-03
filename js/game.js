41bf8bfee978fd181fd10dcdd55ae46e7ba12f12
ALMAS <namazbaev2000@gmail.com>
2020-04-03 16:01:21 +0600

    game change

--- js/game.js
+++ js/game.js
@@ -12,3 +12,20 @@ background.src = "ing/flappy_bird_bg.png";
 foreground.src = "ing/flappy_bird_fg.png";
 pipetop.src = "ing/flappy_bird_pipeUp.png";
 pipedown.src = "ing/flappy_bird_pipeBottom.png";
+
+var gap = 90;
+var xposition = 10;
+var yposition = 150;
+var gravity = 1;
+
+function draw(){
+	context.drawImage(background, 0, 0);
+	context.drawImage(pipetop, 100, 0);
+	context.drawImage(pipedown, 100, 0 + pipetop.height + gap);
+	context.drawImage(foreground, 0, canvas.height - foreground.height);
+	context.drawImage(bird, xposition, yposition);
+
+	yposition += gravity;
+	requestAnimationFrame(draw);
+}
+pipedown.onload = draw;
